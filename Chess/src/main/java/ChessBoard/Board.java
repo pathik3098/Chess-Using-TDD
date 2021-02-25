7aed46d300004416690f34e8d459d7cbae8b1eab
Pathik Kumar Patel <pt999986@dal.ca>
2021-02-26 01:06:51 +0530

    piece abstract updated

---
+++ Chess/src/main/java/ChessBoard/Board.java
@@ -0,0 +1,8 @@
+package ChessBoard;
+import Pieces.Piece;
+
+public class Board {
+    public Piece getPiece(int x, int y){
+        return null;
+    }
+}
--- Chess/src/main/java/Pieces/Piece.java
+++ Chess/src/main/java/Pieces/Piece.java
@@ -1,7 +1,63 @@
 package Pieces;
 
-public abstract class Piece {
+import ChessBoard.Board;
 
+public abstract class Piece {
+    private int x,y;
+    private boolean is_white;
+    String path;
+    Board board;
+
+
+    public Piece(int x, int y, boolean is_white, String path, Board board){
+        this.x=x;
+        this.y=y;
+        this.is_white=is_white;
+        this.path = path;
+        this.board = board;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public String getPath()
+    {
+        return path;
+    }
+
+    public void setPath(String path)
+    {
+        this.path = path;
+    }
+
+    public boolean isWhite()
+    {
+        return is_white;
+    }
+
+    public boolean isBlack()
+    {
+        return !is_white;
+    }
+
+    // method to check the valid moves of a piece
     public abstract boolean ValidMove();
 
 }
