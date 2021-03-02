8eb3bbbe9ecb4dd2a19bca2f98801d2887bf7723
Aparna Vivekanandan <ap777442@dal.ca>
2021-03-02 17:43:04 +0530

    Initial commit

--- Chess/src/main/java/Piece/Pawn.java
+++ Chess/src/main/java/Piece/Pawn.java
@@ -1,89 +1,73 @@
 package Piece;
-
 import java.util.ArrayList;
-public class Pawn
+
+public class Pawn extends piece
 {
-    //Assuming the current board status as of now
-    char[][] current_board_state = {    {'r','n','b','q','k','b','n','r'},
-                                        {'p','p','p','p','0','p','p','p'},
-                                        {'0','0','0','0','p','0','0','0'},
-                                        {'0','0','0','0','0','0','0','0'},
-                                        {'0','0','0','0','0','0','0','0'},
-                                        {'0','0','0','P','0','0','0','0'},
-                                        {'P','P','P','0','P','P','P','P'},
-                                        {'R','N','B','Q','K','B','N','R'}
-                                    };
-
-    ArrayList<Integer> possible_moves = new ArrayList<Integer>(5);
-
-    //Destination x & y - where the user wants to move
-    private int Des_x;
-    private int Des_y;
-
-    //Current x & y position of the pawn piece - which the user wants to move
-    private int Cur_x;
-    private int Cur_y;
-
-    //piece_colour_indicator
-    private int c;
-
-    //Initialising
-    boolean pawn_moved = false;
-
-
-    public void getDesPositions(int x, int y)
+    public void cal_Possible_Moves_white(int Cur_x, int Cur_y)
     {
-        this.Des_x = x;
-        this.Des_y = y;
-    }
-
-    public void getCurrentPositions(int a, int b)
-    {
-        this.Cur_x = a;
-        this.Cur_y = b;
-    }
-    public int iswhiteOrblack(String colour)
-    {
-        if (colour.equals("white"))
+        if (c == 1) //White
         {
-             c = 1;
-            return c;
-        }
-        else
-        {
-             c = 0;
-            return c;
-        }
-    }
-
-    public int[][] cal_Possible_Moves(int Cur_x, int Cur_y)
-    {
-
-        if ((pawn_moved == false) && (c == 1))
-        {
-            if ((current_board_state[Cur_x - 1][Cur_y] =='0') && (current_board_state[Cur_x - 2][Cur_y] =='0'))
+            if (pawn_moved == false) // Pawn piece hasn't made its first move
             {
-                possible_moves.add(current_board_state[Cur_x - 1][Cur_y]);
-                possible_moves.add(current_board_state[Cur_x - 2][Cur_y]);
-                pawn_moved = true;
+                if ((current_board_state[Cur_x - 1][Cur_y] == '0') && (current_board_state[Cur_x - 2][Cur_y] == '0')) {
+                    ValidMove(Cur_x - 1, Cur_y);
+                    ValidMove(Cur_x - 2, Cur_y);
+                    pawn_moved = true;
+                } else if ((current_board_state[Cur_x - 1][Cur_y] == '0') && (current_board_state[Cur_x - 2][Cur_y] != '0')) {
+                    ValidMove(Cur_x - 1, Cur_y);
+                } else {
+                    //Cannot move: Pawn cannot Jump and move to the next
+                    //Cannot move: Pawn cannot move front as the front places are occupied with other pieces.
+                    System.out.println("Cannot move the pawn: The destination path is blocked");
+                }
             }
-
+            else { ValidMove(Cur_x - 1, Cur_y); }
         }
-        else if((pawn_moved == false) && (c == 0))
+    }
+
+
+    public void cal_Possible_Moves_black(int Cur_x, int Cur_y)
+    {
+        if (c == 0) // Black
         {
-            if ((current_board_state[Cur_x + 1][Cur_y] =='0') && (current_board_state[Cur_x + 2][Cur_y] =='0'))
+            if (pawn_moved == false) // Pawn piece hasn't made its first move
             {
-                possible_moves.add(current_board_state[Cur_x + 1][Cur_y]);
-                possible_moves.add(current_board_state[Cur_x + 2][Cur_y]);
-                pawn_moved = true;
+                if ((current_board_state[Cur_x + 1][Cur_y] == '0') && (current_board_state[Cur_x + 2][Cur_y] == '0')) {
+                    ValidMove(Cur_x + 1, Cur_y);
+                    ValidMove(Cur_x + 2, Cur_y);
+                    pawn_moved = true;
+                } else if ((current_board_state[Cur_x + 1][Cur_y] == '0') && (current_board_state[Cur_x + 2][Cur_y] != '0')) {
+                    ValidMove(Cur_x + 1, Cur_y);
+                } else {
+                    //Cannot move: Pawn cannot Jump and move to the next
+                    //Cannot move: Pawn cannot move front as the front places are occupied with other pieces.
+                    System.out.println("Cannot move the pawn: The destination path is blocked");
+                }
+
+            } else {
+                ValidMove(Cur_x + 1, Cur_y);
             }
         }
+    }
+
+    public void attack_move(int Cur_x, int Cur_y)
+    {
+
     }
 
     //x & y are the destinations
-    public boolean canMove(int x, int y)
+    public boolean ValidMove(int x, int y)
     {
-
-        return true;
+        if ((x == Des_x) && (y == Des_y) )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
+
+
+    
 }
--- Chess/src/main/java/Piece/piece.java
+++ Chess/src/main/java/Piece/piece.java
@@ -1,6 +1,62 @@
 package Piece;
 
+import java.util.ArrayList;
+
 abstract class piece
 {
-    // To be Coded
+    //Assuming the current board status as of now
+    char[][] current_board_state = {    {'r','n','b','q','k','b','n','r'},
+            {'p','p','p','p','0','p','p','p'},
+            {'0','0','0','0','p','0','0','0'},
+            {'0','0','0','0','0','0','0','0'},
+            {'0','0','0','0','0','0','0','0'},
+            {'N','0','0','P','0','0','0','0'},
+            {'P','P','P','0','P','P','P','P'},
+            {'R','0','B','Q','K','B','N','R'}
+    };
+
+    ArrayList<Integer> possible_moves = new ArrayList<Integer>(5);
+
+    //Destination x & y - where the user wants to move
+    public int Des_x;
+    public int Des_y;
+
+    //Current x & y position of the pawn piece - from which the user wants to move
+    public int Cur_x;
+    public int Cur_y;
+
+    //piece_colour_indicator
+    public int c;
+
+    //Initialising
+    boolean pawn_moved = false;
+
+
+    public void getDesPositions(int x, int y)
+    {
+        this.Des_x = x;
+        this.Des_y = y;
+    }
+
+    public void getCurrentPositions(int a, int b)
+    {
+        this.Cur_x = a;
+        this.Cur_y = b;
+    }
+
+    public int iswhiteOrblack(String colour)
+    {
+        if (colour.equals("white"))
+        {
+            c = 1;
+            return c;
+        }
+        else
+        {
+            c = 0;
+            return c;
+        }
+    }
+
+    public abstract boolean ValidMove(int target_x, int target_y);
 }
