dcd58dec09a47e364d5ec0a0ad46aaf72debd4a4
Pathik Kumar Patel <pt999986@dal.ca>
2021-03-19 03:13:12 +0530

    Completed rook checking conditions

--- Chess/src/main/java/Pieces/Rook.java
+++ Chess/src/main/java/Pieces/Rook.java
@@ -2,6 +2,8 @@ package Pieces;
 
 import ChessBoard.Board;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Rook extends Piece{
 
@@ -13,23 +15,22 @@ public class Rook extends Piece{
     public boolean ValidMove(int destination_x, int destination_y) {
 
         Piece piece = board.getPiece(destination_x,destination_y);
-        ArrayList Conditions = new ArrayList();
+        List Conditions = new ArrayList();
 
         Conditions.add(differentColourPiece(this,piece));
         Conditions.add(straightMovement(this,piece));
         Conditions.add(isPieceInBetween(this,piece));
 
+        Iterator iter = Conditions.iterator();
+        while(iter.hasNext())
+        {
+
+        }
         return false;
     }
 
     public boolean straightMovement(Piece currentPiece, Piece targetPiece) {
-
-        if((currentPiece.getPositionX()==targetPiece.getPositionX())||(currentPiece.getPositionY()==targetPiece.getPositionY())){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (currentPiece.getPositionX() == targetPiece.getPositionX()) || (currentPiece.getPositionY() == targetPiece.getPositionY());
     }
 
     public boolean isPieceInBetween(Piece currentPiece, Piece targetPiece){
@@ -39,26 +40,67 @@ public class Rook extends Piece{
         int targetX = targetPiece.getPositionX();
         int targetY = targetPiece.getPositionY();
 
-        int signX = Integer.signum(targetX - currentX);
-        int signY = Integer.signum(targetY - currentY);
+        int directionSignX = Integer.signum(targetX - currentX);
+        int directionSignY = Integer.signum(targetY - currentY);
 
-        int possibleX = currentX + (signX);
-        int possibleY = currentY + (signY);
-
-        while((targetX != currentX) && (targetY != currentY))
+        if(directionSignX > 0)
         {
-            // here the first current position will be of the active piece itself,
-            // so try to find a solution to that problem.
+            int spaceToMove = Math.abs(targetX - currentX);
 
-            Piece p = board.getPiece(currentX,currentY);
-
-            if(p != null)
+            for (int i=1 ; i < spaceToMove ; i++)
             {
-                return  false;
-            }
+                Piece p = board.getPiece(currentX + i,currentY);
 
-            currentX = currentX + (signX);
-            currentY = currentY + (signY);
+                if(p != null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(directionSignX < 0)
+        {
+            int spaceToMove = Math.abs(targetX - currentX);
+
+            for (int i=1 ; i < spaceToMove ; i++)
+            {
+                Piece p = board.getPiece(currentX - i,currentY);
+
+                if(p != null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(directionSignY > 0)
+        {
+            int spaceToMove = Math.abs(targetY - currentY);
+
+            for (int i=1 ; i < spaceToMove ; i++)
+            {
+                Piece p = board.getPiece(currentX,currentY + i);
+
+                if(p != null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(directionSignY < 0)
+        {
+            int spaceToMove = Math.abs(targetY - currentY);
+
+            for (int i=1 ; i < spaceToMove ; i++)
+            {
+                Piece p = board.getPiece(currentX - i,currentY);
+
+                if(p != null)
+                {
+                    return false;
+                }
+            }
         }
         return true;
     }
