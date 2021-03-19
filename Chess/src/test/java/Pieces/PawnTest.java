e6cdb2b7260e563899f3eb0b76e01c96797a8964
Aparna Vivekanandan <ap777442@dal.ca>
2021-03-19 12:05:34 +0530

    Testcases

---
+++ Chess/src/test/java/Pieces/PawnTest.java
@@ -0,0 +1,19 @@
+package Pieces;
+
+import ChessBoard.Board;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PawnTest {
+
+    Board boardObject = new Board();
+
+    // Valid move test when null
+    @Test
+    void ValidMoveTest() {
+        Pawn pawnObject = new Pawn(1, 1, true, "xxx", boardObject, true);
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            pawnObject.validMove(1, 2);
+        });
+    }
+}
