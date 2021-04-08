ce5dd0825a81e290c7cee71e6652f0c759f9e9ab
Darshilkumar Kanjibhai Rijiya <dr938088@dal.ca>
2021-04-08 15:10:37 -0300

    Update IPersistenceConnection.java for connection

--- Chess/src/main/java/com/tournament/persistenceconnection/IPersistenceConnection.java
+++ Chess/src/main/java/com/tournament/persistenceconnection/IPersistenceConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 
 public interface IPersistenceConnection
 {
-    String connectionUrl = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_12_TEST";
+    String connectionUrl = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_12_TEST?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     String username = "CSCI5308_12_TEST_USER";
     String password = "Fv297LbSgareuWQf";
 
